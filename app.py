1eb0e6ecdbb0c4cee3439f54aef5841ab4c504ba
swethasingireddy <swethasingireddy@usf.edu>
2025-05-25 00:02:06 -0400

    Updating  app.py

--- app.py
+++ app.py
@@ -133,5 +133,6 @@ def predict():
         return jsonify({'error': 'Prediction error.'}), 500
 
 if __name__ == '__main__':
-   
-    pass
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
